da575a8a1c58c939f9a11ffef0c3f6b1f0085846
Théo Prigent <tprigent@insa-rennes.fr>
2021-02-07 18:35:56 +0100

    Error fixed: executable changed from main_UI.c to main.c in CMakeList.txt

---
+++ cmake-build-debug/CMakeFiles/main.c.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/theoprigent/OneDrive/Ecole/04_INSA/3EII/S6/MCL/logiciel-kine2/gtk_functions.c" "/Users/theoprigent/OneDrive/Ecole/04_INSA/3EII/S6/MCL/logiciel-kine2/cmake-build-debug/CMakeFiles/main.c.dir/gtk_functions.c.o"
+  "/Users/theoprigent/OneDrive/Ecole/04_INSA/3EII/S6/MCL/logiciel-kine2/main.c" "/Users/theoprigent/OneDrive/Ecole/04_INSA/3EII/S6/MCL/logiciel-kine2/cmake-build-debug/CMakeFiles/main.c.dir/main.c.o"
+  "/Users/theoprigent/OneDrive/Ecole/04_INSA/3EII/S6/MCL/logiciel-kine2/main_UI.c" "/Users/theoprigent/OneDrive/Ecole/04_INSA/3EII/S6/MCL/logiciel-kine2/cmake-build-debug/CMakeFiles/main.c.dir/main_UI.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/libffi/3.3_2/include"
+  "/usr/local/Cellar/gtk+3/3.24.24/include/gtk-3.0"
+  "/usr/local/Cellar/glib/2.66.4_1/include/gio-unix-2.0"
+  "/usr/local/Cellar/cairo/1.16.0_4/include/cairo"
+  "/usr/local/Cellar/libepoxy/1.5.5_1/include"
+  "/usr/local/Cellar/pango/1.48.1/include/pango-1.0"
+  "/usr/local/Cellar/harfbuzz/2.7.4/include/harfbuzz"
+  "/usr/local/Cellar/fribidi/1.0.10/include/fribidi"
+  "/usr/local/Cellar/graphite2/1.3.14/include"
+  "/usr/local/Cellar/atk/2.36.0/include/atk-1.0"
+  "/usr/local/Cellar/pixman/0.40.0/include/pixman-1"
+  "/usr/local/Cellar/fontconfig/2.13.1/include"
+  "/usr/local/opt/freetype/include/freetype2"
+  "/usr/local/Cellar/libpng/1.6.37/include/libpng16"
+  "/usr/local/Cellar/gdk-pixbuf/2.42.2/include/gdk-pixbuf-2.0"
+  "/usr/local/Cellar/glib/2.66.4_1/include"
+  "/usr/local/Cellar/glib/2.66.4_1/include/glib-2.0"
+  "/usr/local/Cellar/glib/2.66.4_1/lib/glib-2.0/include"
+  "/usr/local/opt/gettext/include"
+  "/usr/local/Cellar/pcre/8.44/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/main.c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.c.dir/gtk_functions.c.o"
+  "CMakeFiles/main.c.dir/main.c.o"
+  "CMakeFiles/main.c.dir/main_UI.c.o"
+  "main.c"
+  "main.c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
