0b1439b90cb3dbeffae750986e634297519381ce
Théo Prigent <tprigent@insa-rennes.fr>
2021-02-08 12:36:26 +0100

    Main view: add edit button for folder zone

--- gtk_functions.c
+++ gtk_functions.c
@@ -20,46 +20,48 @@ void createPatientInfoWindow(GtkWidget *box){
 }
 
 void createFolderInfoWindow(GtkWidget *box){
-    /* Create a box which contains the separator and a vbox ************************* */
+    /* Create a grid which contains the different elements of the folder ************ */
     GtkWidget *grid_part2 = NULL;
     grid_part2 = gtk_grid_new();
     gtk_box_pack_start(GTK_BOX(box), grid_part2, TRUE, TRUE, 0);
     /* ****************************************************************************** */
 
-    /* Section which contains medical folder **************************************** */
+    /* Create a frame for the folder zone ******************************************* */
     GtkWidget *cadre_folder = NULL;
     cadre_folder = gtk_frame_new("Dossier");
     gtk_grid_attach(GTK_GRID(grid_part2), cadre_folder, GTK_ALIGN_START, GTK_ALIGN_START, 1, 1);
     gtk_widget_set_hexpand(cadre_folder, TRUE);
     gtk_widget_set_vexpand(cadre_folder, TRUE);
+    /* ****************************************************************************** */
 
-    /* Create a grid to display all the elements */
+    /* Create a grid to display all the elements ************************************ */
     GtkWidget *grid_folder = NULL;
     grid_folder = gtk_grid_new();
     gtk_container_add(GTK_CONTAINER(cadre_folder), grid_folder);
+    /* ****************************************************************************** */
 
 
     /* Display the folder title ***************************************************** */
     /* BOX */
-    GtkWidget *vbox_folder = NULL;
-    vbox_folder = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-    gtk_grid_attach(GTK_GRID(grid_folder), vbox_folder, GTK_ALIGN_CENTER, GTK_ALIGN_START, 3, 1);
+    GtkWidget *hbox_folder = NULL;
+    hbox_folder = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+    gtk_grid_attach(GTK_GRID(grid_folder), hbox_folder, GTK_ALIGN_CENTER, GTK_ALIGN_START, 3, 1);
 
     /* CONSTRAINTS */
-    gtk_widget_set_hexpand(vbox_folder, TRUE);
-    gtk_widget_set_vexpand(vbox_folder, FALSE);
-    gtk_widget_set_halign(vbox_folder, GTK_ALIGN_CENTER);
+    gtk_widget_set_hexpand(hbox_folder, TRUE);
+    gtk_widget_set_vexpand(hbox_folder, FALSE);
+    gtk_widget_set_halign(hbox_folder, GTK_ALIGN_CENTER);
 
     /* MARGINS */
-    gtk_widget_set_margin_top(vbox_folder, 10);
-    gtk_widget_set_margin_bottom(vbox_folder, 40);
+    gtk_widget_set_margin_top(hbox_folder, 10);
+    gtk_widget_set_margin_bottom(hbox_folder, 40);
 
     /* LABEL */
     GtkWidget *folder_title = NULL;
     folder_title = gtk_label_new("<big><b>** CURRENT FOLDER **</b></big>");   //TODO: make this text dynamic
     gtk_label_set_use_markup(GTK_LABEL(folder_title), TRUE);
     gtk_widget_set_halign(folder_title, GTK_ALIGN_CENTER);
-    gtk_box_pack_start(GTK_BOX(vbox_folder), folder_title, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(hbox_folder), folder_title, FALSE, FALSE, 0);
 
     /* ****************************************************************************** */
 
@@ -67,7 +69,7 @@ void createFolderInfoWindow(GtkWidget *box){
     /* BOX */
     GtkWidget *hbox_treatment = NULL;
     hbox_treatment = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-    gtk_grid_attach_next_to(GTK_GRID(grid_folder), hbox_treatment, vbox_folder, GTK_POS_BOTTOM, 1, 1);
+    gtk_grid_attach_next_to(GTK_GRID(grid_folder), hbox_treatment, hbox_folder, GTK_POS_BOTTOM, 1, 1);
 
     /* CONSTRAINTS */
     gtk_widget_set_hexpand(hbox_treatment, TRUE);
@@ -143,8 +145,6 @@ void createFolderInfoWindow(GtkWidget *box){
 
     /* ****************************************************************************** */
 
-    /* ****************************************************************************** */
-
     /* See attachments ************************************************************** */
     /* BOX */
     GtkWidget *hbox_attachments = NULL;
@@ -170,6 +170,28 @@ void createFolderInfoWindow(GtkWidget *box){
     gtk_widget_set_hexpand(attachments_button, FALSE);
     gtk_widget_set_vexpand(attachments_button, FALSE);
     gtk_box_pack_start(GTK_BOX(hbox_attachments), attachments_button, FALSE, FALSE, 0);
+
+    /* ****************************************************************************** */
+
+    /* Edit button ****************************************************************** */
+    /* BOX */
+    GtkWidget *hbox_edit_folder = NULL;
+    hbox_edit_folder = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+    gtk_grid_attach_next_to(GTK_GRID(grid_folder), hbox_edit_folder, hbox_folder, GTK_POS_RIGHT, 1, 1);
+
+    /* CONSTRAINTS */
+    gtk_widget_set_hexpand(hbox_edit_folder, FALSE);
+    gtk_widget_set_vexpand(hbox_edit_folder, FALSE);
+    gtk_widget_set_halign(hbox_edit_folder, GTK_ALIGN_END);
+    gtk_widget_set_valign(hbox_edit_folder, GTK_ALIGN_END);
+
+    /* BUTTON */
+    GtkWidget *edit_folder_button = NULL;
+    edit_folder_button = gtk_button_new_from_icon_name("document-open", GTK_ICON_SIZE_MENU);
+    //edit_folder_button = gtk_button_new_with_label("Éditer");
+    gtk_widget_set_hexpand(edit_folder_button, FALSE);
+    gtk_widget_set_vexpand(edit_folder_button, FALSE);
+    gtk_box_pack_start(GTK_BOX(hbox_edit_folder), edit_folder_button, FALSE, FALSE, 0);
 
     /* ****************************************************************************** */
 
