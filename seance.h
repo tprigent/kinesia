26133fe21b63012188e5072afc34ac39c9ed5c11
Paul Bertho <paul.bertho@insa-rennes.fr>
2021-02-13 18:38:02 +0100

    Correction conflits

--- seance.h
+++ seance.h
@@ -1,28 +1,15 @@
 //
-<<<<<<< HEAD
 // Created by paul on 12/02/2021.
-=======
-// Created by paul on 12/02/2021.
->>>>>>> gestion-bdd
 //
 
 #ifndef LOGICIEL_KINE_SEANCE_H
 #define LOGICIEL_KINE_SEANCE_H
 
-<<<<<<< HEAD
 #include <stdio.h>
 #include "Structures.h"
 #include "sqlite3.h"
 
 //Ajout d'une seance avec un instance de struct seance
 int addSeance(Seance *seance);
-=======
-#include <stdio.h>
-#include "Structures.h"
-#include "sqlite3.h"
-
-//Ajout d'une seance avec un instance de struct seance
-int addSeance(Seance *seance);
->>>>>>> gestion-bdd
 
 #endif //LOGICIEL_KINE_SEANCE_H
