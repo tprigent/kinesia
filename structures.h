9511a2b79a630c4776e4c234a56d989b05acd321
Théo Prigent <tprigent@insa-rennes.fr>
2021-02-15 19:07:57 +0100

    Folder structure added

--- structures.h
+++ structures.h
@@ -67,4 +67,12 @@ typedef struct{
     unsigned int idDossier;
 }Seance;
 
+typedef struct{
+    char *folderName;           //Name of the folder
+    char *pathology;            //Reason for consultation
+    char *details;              //Important things to know about the pathology
+    unsigned int numberOfFiles; //To count the attached files
+    unsigned int idFolder;      //Unique number for a folder
+}Folder;
+
 #endif //LOGICIEL_KINE_STRUCTURES_H
