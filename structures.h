2d0c64431b4def495ddd1fdf77ea596aa276e343
Théo Prigent <tprigent@insa-rennes.fr>
2021-02-23 15:59:21 +0100

    Documentation: strutures documented

--- structures.h
+++ structures.h
@@ -9,71 +9,104 @@
 #define LG_MAX_INFO 100
 #define LG_MAX_OTHERS 300
 
+/**
+ * \brief Structure to represent a Date
+*/
 typedef struct{
-    int day;
-    int month;
-    int year;
+    int day;              /**< the day */
+    int month;            /**< the month (1 to 12) */
+    int year;             /**< the year (4-digits) */
 }Date;
 
-/*enum pour genre d'une personne*/
+
+
+/**
+ * \brief The enumeration of human genders
+*/
 typedef enum {MAN, WOMAN, OTHER}Genre;
 
 
-/*Structure adresse*/
+/**
+ * \brief Structure to represent an Address
+*/
 typedef struct {
-    char * number; //la personne doit ajouter bis ou ter si maison bis ou ter (à préciser dans l'interface)
-    char * street; //Rue ou avenue ou autre
-    char * postCode; //code postal
-    char * city;
-    char * other_info; //num batiment, num appartement...
+    char * number;                  /**< street number (add bis or ter if necessary) */
+    char * street;                  /**< name of the street, avenue, etc */
+    char * postCode;                /**< number of digit can depend */
+    char * city;                    /**< city name */
+    char * other_info;              /**< floor number, building number (if applicable) */
 }Address;
 
+
+
+/**
+ * \brief Structure to represent a Patient
+*/
 typedef struct{
-    /* Etat civil */
-    char * name;
-    char * firstname;
-    Date birthdate;
-    char * place_birth;
-    Genre gender;       // Enum HOMME, FEMME, AUTRE
-    Address address;
-    char * phone_number;
-    char * mail_address;
-    char * job;
-    char * ssn;     // Num de secu trop grand meme pour un long int
+    /**
+      * \name Civil status
+    */
+    char * name;                   /**< Patient name */
+    char * firstname;              /**< Patient first */
+    Date birthdate;                /**< Patient birthdate */
+    char * place_birth;            /**< Patient place of birth (city) */
+    Genre gender;                  /**< Patient gender (enum) */
+    Address address;               /**< Patient address */
+    char * phone_number;           /**< Patient phone number */
+    char * mail_address;           /**< Patient e-mail */
+    char * job;                    /**< Patient job that might be useful for a better understanding of some health issues */
+    char * ssn;                    /**< Patient social security number (too long for an int) */
+    /**
+      * \name Medical information
+    */
+    int weight;                   /**< Patient weight (in kg) */
+    int height;                   /**< Patient height (in cm) */
+    Date first_consultation;      /**< first consultation at this office */
+    char * global_pathologies;    /**< pathologies that might be useful for a better understanding of some health issues */
 
-    /* Informations médicales */
-    int weight;
-    int height;
-    Date first_consultation;
-    char * global_pathologies;
-
-    /* Autre */
-    unsigned int id;
+    /**
+      * \name Database information
+    */
+    unsigned int id;             /**< Patient unique identifier to select him in database */
 
 }Patient;
 
 
+/**
+ * \brief Structure to represent a Session
+*/
 typedef struct{
-    /*Informations relatives à la séance*/
-    char * sessionName; //Titre de la séance
-    Date sessionDate; //Date de la séance actuelle
-    Date nextSessionDate; //Date de la prochaine séance
-    char * observations; //observations faites lors de la séance
-
-    /*id Séance (je sais pas si ce sera utile mais il paraît qu'il faut toujours faire ça*/
-    unsigned int idSession;
-
-    /*lien à un dossier*/
-    unsigned int idFolder;
+    /**
+      * \name Session relative information
+    */
+    char * sessionName;         /**< Session title (eventually chosen by user) */
+    Date sessionDate;           /**< Session date */
+    Date nextSessionDate;       /**< Date of the next session */
+    char * observations;        /**< Observations made during the session */
+    /**
+     * \name Database information
+    */
+   unsigned int idSession;      /**< Session unique identifier to select it in database */
+    unsigned int idFolder;      /**< Related Folder identifier */
 }Session;
 
+
+/**
+ * \brief Structure to represent a Folder
+*/
 typedef struct{
-    char *folderName;           //Name of the folder
-    char *pathology;            //Reason for consultation
-    char *details;              //Important things to know about the pathology
-    Date startOfTreatment;      //Date of the beginning of the treatment
-    unsigned int numberOfFiles; //To count the attached files
-    unsigned int idFolder;      //Unique number for a folder
+    /**
+      * \name Folder relative information
+    */
+    char *folderName;           /**< Folder name */
+    char *pathology;            /**< Reason for consultation */
+    char *details;              /**< Important things to know about the pathology */
+    Date startOfTreatment;      /**< Date of the beginning of the treatment */
+    unsigned int numberOfFiles; /**< Attachment counter */
+    /**
+     * \name Database information
+    */
+    unsigned int idFolder;      /**< Folder unique identifier to select it in database */
 }Folder;
 
 #endif //LOGICIEL_KINE_STRUCTURES_H
