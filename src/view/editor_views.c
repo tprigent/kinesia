b7898d5f8bd5a6bb57a52f1018228fbc80e5f250
Julien Priam <jpriam@insa-rennes.fr>
2021-03-12 23:11:39 +0100

    UI-editors_view : increase adress size

--- src/view/editor_views.c
+++ src/view/editor_views.c
@@ -276,7 +276,7 @@ void launchPatientEditor(GtkWidget *but_edit, Patient_window *patient_window){
     gtk_entry_set_max_length(GTK_ENTRY(surname_entry), 30);
     gtk_entry_set_max_length(GTK_ENTRY(birth_entry), 10);
     gtk_entry_set_max_length(GTK_ENTRY(job_entry), 10);
-    gtk_entry_set_max_length(GTK_ENTRY(address_entry), 30);
+    gtk_entry_set_max_length(GTK_ENTRY(address_entry), 60);
     gtk_entry_set_max_length(GTK_ENTRY(postcode_entry), 5);
     gtk_entry_set_max_length(GTK_ENTRY(city_entry), 25);
     gtk_entry_set_max_length(GTK_ENTRY(number_entry), 11);
