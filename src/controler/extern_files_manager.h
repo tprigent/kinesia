9ad088ddc2f8ad6a2c7b26f2a35747ac97fac6b8
Théo Prigent <tprigent@insa-rennes.fr>
2021-03-13 16:02:06 +0100

    Photo profile management: add photo path finder

--- src/controler/extern_files_manager.h
+++ src/controler/extern_files_manager.h
@@ -13,5 +13,6 @@
 void copyToMedia(char *from, Patient *patient, char *type);
 char *getExtensionFromPath(char *path);
 char *getProfileExtension(Patient *patient);
+char *getProfilePhotoPath(Patient *patient);
 
 #endif //LOGICIEL_KINE_EXTERN_FILES_MANAGER_H
