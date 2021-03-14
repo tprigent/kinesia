f1696d4ace1d64be9e2bc5f25ef00e670a7d331e
Julien Priam <jpriam@insa-rennes.fr>
2021-03-14 13:02:33 +0100

    Doc : Documentation of test functions in UTest_UI

--- src/UTest_UI.c
+++ src/UTest_UI.c
@@ -18,6 +18,15 @@
 #include <setjmp.h>
 #include <cmocka.h>
 
+/*!
+ * \brief Setup function which allocates and return a patient
+ *
+ * This function get the patient with id 1 (Claude Francois)
+ * and pass its adress in *state for the next tests
+ *
+ * \param[in] A pointer adress to pass the allocated patient
+ * \param[out] An int to know if patient is well allocated
+*/
 static int setup_patient(void **state){
     Patient *patient = getPatient(1);
     if(patient == NULL) return -1;
@@ -27,47 +36,111 @@ static int setup_patient(void **state){
 	return 0;
 }
 
+/*!
+ * \brief Setup function which allocates and fill a patient
+ *
+ * This function gets the patient with id 1 (Claude Francois)
+ * and pass its adress in *state for the next tests
+ *
+ * \param[in] A pointer adress to pass the allocated patient
+ * \param[out] An int to know if patient is well allocated
+*/
 static void test_get_name_UI(void **state){
     Patient * patient = (Patient *) (*state);
     assert_string_equal("Francois Claude", get_name_UI(patient));
 }
 
+/*!
+ * \brief test get_date_UI function
+ *
+ * This function tests get_date_UI which returns a char* "xx/xx/xxxx" passing a patient Birthdate
+ *
+ * \param[in] A pointer adress to pass the allocated patient
+*/
 static void test_get_date_UI(void **state){
     Patient * patient = (Patient *) (*state);
     assert_string_equal("1/2/1939", get_date_UI(&patient->birthdate));
 }
 
+/*!
+ * \brief test get_height_UI function
+ *
+ * This function tests get_height_UI which returns a char* "x.xxm" passing a patient
+ *
+ * \param[in] A pointer adress to pass the allocated patient
+*/
 static void test_get_height_UI(void **state){
     Patient * patient = (Patient *) (*state);
     assert_string_equal("1.70m", get_height_UI(patient));
 }
 
+/*!
+ * \brief test get_weight_UI function
+ *
+ * This function tests get_weight_UI which returns a char* "xxkg" passing a patient
+ *
+ * \param[in] A pointer adress to pass the allocated patient
+*/
 static void test_get_weight_UI(void **state){
     Patient * patient = (Patient *) (*state);
     assert_string_equal("59kg", get_weight_UI(patient));
 }
 
+/*!
+ * \brief test get_height_weight_UI function
+ *
+ * This function tests get_height_weight_UI which returns a char* "xxkg   ;   x.xxm" passing a patient
+ *
+ * \param[in] A pointer adress to pass the allocated patient
+*/
 static void test_get_height_weight_UI(void **state){
     Patient * patient = (Patient *) (*state);
     assert_string_equal("59kg   ;   1.70m", get_height_weight_UI(patient));
 }
 
+/*!
+ * \brief test get_first_consultation_UI function
+ *
+ * This function tests get_first_consultation_UI which returns a char* "Première consultation  : xx/xx/xxxx" passing a patient
+ *
+ * \param[in] A pointer adress to pass the allocated patient
+*/
 static void test_get_first_consultation_UI(void **state){
     Patient * patient = (Patient *) (*state);
     assert_string_equal("Première consultation : 1/1/1962", get_first_consultation_UI(patient));
 }
 
+/*!
+ * \brief test test_get_adress_UI function
+ *
+ * This function tests test_get_adress_UI which returns a char* "number street" passing a patient
+ *
+ * \param[in] A pointer adress to pass the allocated patient
+*/
 static void test_get_adress_UI(void **state){
     Patient * patient = (Patient *) (*state);
     assert_string_equal("2 Rue tintintin", get_adress_UI(patient));
 }
 
+/*!
+ * \brief Teardown function which desallocates the patient
+ *
+ * This function desallocates the patient allocated by setup function
+ *
+ * \param[in] A pointer adress to pass the allocated patient
+ * \param[out] An int to tell that desallocation worked
+*/
 static int teardown(void **state){
     Patient * patient = (Patient *) (*state);
     freePatient(&patient);
     return 0;
 }
 
+/*!
+ * \brief main function which runs the tests for UI
+ *
+ * \param[out] An int to tell if tests are passed
+*/
 int main_UI(void)
 {
 
