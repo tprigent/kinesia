6737abb14723a3bd49bb782978cc96c91154c3d3
Paul Bertho <paul.bertho@insa-rennes.fr>
2021-03-22 20:56:20 +0100

    Ajout getfolder

--- src/controller/BDD_to_struct_folder.h
+++ src/controller/BDD_to_struct_folder.h
@@ -5,4 +5,8 @@
 #ifndef LOGICIEL_KINE_BDD_TO_STRUCT_FOLDER_H
 #define LOGICIEL_KINE_BDD_TO_STRUCT_FOLDER_H
 
+#include "../model/structures.h"
+
+Folder* getFolder(int idFolder);
+
 #endif //LOGICIEL_KINE_BDD_TO_STRUCT_FOLDER_H
