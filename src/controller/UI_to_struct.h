b233ddc6d8b74e6c956b372185648895af8f2f36
Julien Priam <jpriam@insa-rennes.fr>
2021-03-24 12:02:35 +0100

    Controller UI_to_struct : add NewSessionEntries struct

--- src/controller/UI_to_struct.h
+++ src/controller/UI_to_struct.h
@@ -3,6 +3,7 @@
 * \brief File with functions used to parse data from UI entries to structures
 */
 
+#include <gtk/gtk.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,6 +12,13 @@
 
 #ifndef LOGICIEL_KINE_CONNECT_UI_STRUCT_H
 #define LOGICIEL_KINE_CONNECT_UI_STRUCT_H
+
+typedef struct{
+    GtkWidget *sessionName;
+    GtkWidget *sessionDate;
+    GtkWidget *nextSessionDate;
+    GtkWidget *observations;
+}NewSessionEntries;
 
 Date *parseDate(char *stringDate);
 
