6cd90d13eac60f17bb17752da18573c4e47effd1
sguinaud <salome.guinaudeau@insa-rennes.fr>
2021-03-24 09:48:23 +0100

    Removed number attributes in address struct, and modified functions using this number.

--- src/model/patient_manager.h
+++ src/model/patient_manager.h
@@ -11,20 +11,20 @@
 
 /*A changer : faire une fonction d'allocation int allocate(Patient * p) où la structure et les attributs dans la structure sont alloués, et des fonctions de remplissage ensuite*/
 
-/*affichage console d'un patient*/
+
 void printPatient(Patient *patient, char *context);
 void printGender(Genre gender);
 
-/* fonctions d'allocation des structures*/
+
 int allocateStringPatient(char **string, int lg);
 int allocatePatient(Patient ** p);
 int allocateAddress(Address * a);
 void freePatient(Patient ** p);
 void freeAddress(Address * a);
 
-/*fonction de remplissage/modification des attributs des structures*/
+
 int setPatient(Patient * p, char * name, char * fn, Date bd, char * placeBirth, int g, Address ad, char* pn, char * ma, char* job, char * ns, char * w, char * h, Date fc, char * gp,unsigned int idPatient,int isArchived);
-int setAddress(Address * adr, char * numM, char * r, char * cp, char * v, char * iC);
+int setAddress(Address * adr, char * r, char * cp, char * v, char * iC);
 int setDate(Date * d, int j, int m, int a);
 
 
