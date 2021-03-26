95880bb6043919e9b429f246bdfaef41dc70442c
Julien Priam <jpriam@insa-rennes.fr>
2021-03-26 21:45:32 +0100

    UI home_view : move load_css function from main_UI to home_view

--- src/view/main_UI.c
+++ src/view/main_UI.c
@@ -6,30 +6,6 @@
 #include <gtk/gtk.h>
 #include "home_view.h"
 
-/*!
- * \brief Function that launch style.css to modify application style
- *
- * This function set necessary parameters to load css and use css file
- *
-*/
-static void load_css(){
-    GtkCssProvider *provider;
-    GdkDisplay *display;
-    GdkScreen *screen;
-
-    const gchar *css_style_file = "../src/view/style.css";
-    GFile *css_fp = g_file_new_for_path(css_style_file);
-    GError *error = 0;
-
-    provider = gtk_css_provider_new();
-    display = gdk_display_get_default();
-    screen = gdk_display_get_default_screen(display);
-
-    gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
-    gtk_css_provider_load_from_file(provider, css_fp, &error);
-
-    g_object_unref(provider);
-}
 
 /*!
  * \brief Function that launch the User Interface
@@ -41,9 +17,6 @@ static void load_css(){
 */
 int main(int argc, char **argv) {
     gtk_init(&argc, &argv);
-
-    gtk_init(NULL, NULL);
-    load_css();
 
     /* Launch the home window*/
     setHomeWindow();
