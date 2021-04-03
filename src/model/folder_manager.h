3b203ab9b93d65c70675d0f23c0985023e6485d2
Julien Priam <jpriam@insa-rennes.fr>
2021-04-03 19:08:27 +0200

    BDD folder_manager : create function createEmptyFolder

--- src/model/folder_manager.h
+++ src/model/folder_manager.h
@@ -14,6 +14,6 @@ int allocateFolder(Folder **d);
 int allocateStringFolder(char **string, int lg);
 
 int setFolder(Folder *d, char * folderName, char * pathology, char * details, int stDay, int stMonth, int stYear, int nbFiles, int idF, int idP);
-
+Folder *createEmptyFolder(int idPatient);
 
 #endif //LOGICIEL_KINE_FOLDER_H
