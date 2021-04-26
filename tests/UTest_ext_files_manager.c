8e49ed4c5554bdb916fec51bc1df924578ac1ebd
Théo Prigent <tprigent@insa-rennes.fr>
2021-04-26 10:17:38 +0200

    Ext files manager: setup tests

--- tests/UTest_ext_files_manager.c
+++ tests/UTest_ext_files_manager.c
@@ -3,3 +3,20 @@
 //
 
 #include "UTest_ext_files_manager.h"
+
+static void getExtensionFromPath(void **state){
+    char *testPath = "../media/graphic-assets/archive_512.png"
+    char *temp = get_name_UI(patient);
+    assert_string_equal("png", temp);
+    free(temp);
+}
+
+
+int main_UI(void)
+{
+    const struct CMUnitTest tests_UI[]=
+            {
+
+            };
+    return cmocka_run_group_tests_name("Test ext_files_manager module",tests_UI,NULL,NULL);
+}
