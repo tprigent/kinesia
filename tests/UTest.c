8dfe0295b66d610a8f79dba4fe0761a0e14408fd
Théo Prigent <tprigent@insa-rennes.fr>
2021-04-26 10:39:23 +0200

    Ext files manager: new tests available

--- tests/UTest.c
+++ tests/UTest.c
@@ -4,6 +4,7 @@
 */
 
 #include "UTest_display_helpers.h"
+#include "UTest_ext_files_manager.h"
 #include "UTest_BDD.h"
 #include "UTest_session.h"
 #include "UTest_BDD_session.h"
@@ -11,6 +12,6 @@
 
 int main(void){
     //int i = main_BDD_session()*main_BDD()*main_UI()*main_session();
-    int i = main_UI();
+    int i = main_UI()*main_ext_files_manager();
     return i;
 }
--- tests/UTest_ext_files_manager.c
+++ tests/UTest_ext_files_manager.c
@@ -3,20 +3,69 @@
 //
 
 #include "UTest_ext_files_manager.h"
+#include "../src/model/structures.h"
+#include "../src/controller/extern_files_manager.h"
+#include "../src/controller/BDD_to_struct_patient.h"
+#include "../src/controller/struct_to_BDD_patient.h"
+#include "../src/model/patient_manager.h"
 
-static void getExtensionFromPath(void **state){
+static void test_getExtensionFromPath(void **state){
     char *testPath = "../media/graphic-assets/archive_512.png"
     char *temp = get_name_UI(patient);
     assert_string_equal("png", temp);
+    free(testPath);
+    free(temp);
+}
+
+static void test_getProfileExtension(void **state){
+    char *temp = getProfileExtension(1);
+    assert_string_equal(".jpeg", temp);
+    free(temp);
+}
+
+static void test_getProfilePhotoPath(void **state){
+    char *temp = getProfilePhotoPath(1);
+    assert_string_equal("../media/patient-data/1/profil.jpeg", temp);
+    free(temp);
+}
+
+static void test_getPatientMediaPath(void **state){
+    char *temp = getPatientMediaPath(1);
+    assert_string_equal("../media/patient-data/1/", temp);
+    free(temp);
+}
+
+static void test_getFolderMediaPath(void **state){
+    char *temp = getFolderMediaPath(1,1);
+    assert_string_equal("../media/patient-data/1/1/", temp);
+    free(temp);
+}
+
+static void test_getNbOfAttachments(void **state){
+    int count = getNbOfAttachments(1,1);
+    assert_int_equal(0, temp);
+}
+
+static void test_replaceWhitespaces(void **state){
+    char *testString = "this is a test string"
+    char *temp = replaceWhitespaces(testString);
+    assert_string_equal("this_is_a_test_string", temp);
+    free(testString);
     free(temp);
 }
 
 
-int main_UI(void)
+int main_ext_files_manager(void)
 {
     const struct CMUnitTest tests_UI[]=
             {
-
+                    cmocka_unit_test(test_getExtensionFromPath),
+                    cmocka_unit_test(test_getProfileExtension),
+                    cmocka_unit_test(test_getProfilePhotoPath),
+                    cmocka_unit_test(test_getPatientMediaPath),
+                    cmocka_unit_test(test_getFolderMediaPath),
+                    cmocka_unit_test(test_getNbOfAttachments),
+                    cmocka_unit_test(test_replaceWhitespaces)
             };
     return cmocka_run_group_tests_name("Test ext_files_manager module",tests_UI,NULL,NULL);
 }
