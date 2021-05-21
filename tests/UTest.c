f932a77b52a9b0b5269eaccfd4f093a90cc9709e
Théo Prigent <tprigent@insa-rennes.fr>
2021-05-21 18:28:18 +0200

    Testing: write tests for managers

--- tests/UTest.c
+++ tests/UTest.c
@@ -8,10 +8,11 @@
 #include "UTest_BDD.h"
 #include "UTest_session.h"
 #include "UTest_BDD_session.h"
+#include "UTest_manager.h"
 
 
 int main(void){
     //int i = main_BDD_session()*main_BDD()*main_UI()*main_session();
-    int i = patient_display_helpers_tests()*folder_display_helpers_tests()*other_display_helpers_tests()*main_ext_files_manager()*main_BDD()*main_BDD_session();
+    int i = patient_display_helpers_tests()*folder_display_helpers_tests()*other_display_helpers_tests()*main_ext_files_manager()*main_BDD()*main_BDD_session()*main_session()*main_manager();
     return i;
 }
---
+++ tests/UTest_manager.h
@@ -0,0 +1,11 @@
+/*!
+* \file UTest_folder_manager.h
+* \brief Functions to test the functions to manage external files
+*/
+
+#ifndef LOGICIEL_KINE_UTEST_MANAGER_H
+#define LOGICIEL_KINE_UTEST_MANAGER_H
+
+int main_manager(void);
+
+#endif //LOGICIEL_KINE_UTEST_MANAGER_H
